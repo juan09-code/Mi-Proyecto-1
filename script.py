828094a8334346a62d375c807bfc12d7a2214b90
Josue Morales <josue_morales_riv11@hotmail.com>
2020-12-21 13:42:16 -1200

    Agregado datos del proyecto

--- script.py
+++ script.py
@@ -2,4 +2,11 @@
 # Autor: Rodrigo Tufiño <rtufino@ups.edu.ec>
 # Fecha: 23-06-2020
 # Versión: 1.0.0
-print("Hola Mundo!")
+
+def main():
+    nombre= input("Cual es tu nombre? ") 
+    print("Hola", nombre, "Bienvenido a Git!")
+
+
+if __name__ == "__main__":
+    main()
